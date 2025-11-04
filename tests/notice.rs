7a5e01227983e22550c7fb6af425ac1ca077d1cb
smiggleworth <jrepanich@gmail.com>
2025-11-04 14:45:31 -0500

    tests

--- tests/notice.rs
+++ tests/notice.rs
@@ -18,7 +18,7 @@ use harness::common::{create_sub_channel, default_sub_capacity, start_test_engin
 async fn should_deliver_notice_to_single_subscriber() {
     // Arrange
     let (handle, _store) = start_test_engine();
-    let (tx, mut rx) = create_sub_channel(default_sub_capacity());
+    let (tx, _rx) = create_sub_channel(default_sub_capacity());
     let _sub = handle
         .subscribe("notice://realm/area/resource/alerts".to_string(), tx, 1)
         .await
@@ -45,8 +45,8 @@ async fn should_deliver_notice_to_single_subscriber() {
 async fn should_deliver_notice_to_multiple_subscribers() {
     // Arrange
     let (handle, _store) = start_test_engine();
-    let (tx1, mut rx1) = create_sub_channel(default_sub_capacity());
-    let (tx2, mut rx2) = create_sub_channel(default_sub_capacity());
+    let (tx1, _rx1) = create_sub_channel(default_sub_capacity());
+    let (tx2, _rx2) = create_sub_channel(default_sub_capacity());
     let _sub1 = handle
         .subscribe("notice://realm/area/resource/alerts".to_string(), tx1, 1)
         .await
@@ -77,7 +77,7 @@ async fn should_deliver_notice_to_multiple_subscribers() {
 async fn should_support_hierarchical_route_matching() {
     // Arrange
     let (handle, _store) = start_test_engine();
-    let (tx, mut rx) = create_sub_channel(default_sub_capacity());
+    let (tx, _rx) = create_sub_channel(default_sub_capacity());
     let _sub = handle
         .subscribe("notice://realm/area/system/alerts".to_string(), tx, 1)
         .await
@@ -104,7 +104,7 @@ async fn should_support_hierarchical_route_matching() {
 async fn should_unsubscribe_successfully() {
     // Arrange
     let (handle, _store) = start_test_engine();
-    let (tx, mut rx) = create_sub_channel(default_sub_capacity());
+    let (tx, _rx) = create_sub_channel(default_sub_capacity());
     let sub_id = handle
         .subscribe("notice://realm/area/resource/test".to_string(), tx, 1)
         .await
@@ -118,30 +118,40 @@ async fn should_unsubscribe_successfully() {
 }
 
 #[tokio::test]
-async fn should_handle_subscribe_with_different_channel_ids() {
+async fn should_subscribe_with_channel_id_one() {
     // Arrange
     let (handle, _store) = start_test_engine();
-    let (tx1, mut rx1) = create_sub_channel(default_sub_capacity());
-    let (tx2, mut rx2) = create_sub_channel(default_sub_capacity());
+    let (tx, _rx) = create_sub_channel(default_sub_capacity());
 
     // Act
-    let sub1 = handle
-        .subscribe("notice://realm/area/ch1/events".to_string(), tx1, 1)
-        .await;
-    let sub2 = handle
-        .subscribe("notice://realm/area/ch2/events".to_string(), tx2, 2)
+    let result = handle
+        .subscribe("notice://realm/area/ch1/events".to_string(), tx, 1)
         .await;
 
     // Assert
-    assert!(sub1.is_ok());
-    assert!(sub2.is_ok());
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn should_subscribe_with_channel_id_two() {
+    // Arrange
+    let (handle, _store) = start_test_engine();
+    let (tx, _rx) = create_sub_channel(default_sub_capacity());
+
+    // Act
+    let result = handle
+        .subscribe("notice://realm/area/ch2/events".to_string(), tx, 2)
+        .await;
+
+    // Assert
+    assert!(result.is_ok());
 }
 
 #[tokio::test]
 async fn should_cleanup_channel_subscriptions() {
     // Arrange
     let (handle, _store) = start_test_engine();
-    let (tx, mut rx) = create_sub_channel(default_sub_capacity());
+    let (tx, _rx) = create_sub_channel(default_sub_capacity());
     let _sub = handle
         .subscribe("notice://realm/area/resource/cleanup".to_string(), tx, 99)
         .await
@@ -158,7 +168,7 @@ async fn should_cleanup_channel_subscriptions() {
 async fn should_deliver_notice_with_metadata() {
     // Arrange
     let (handle, _store) = start_test_engine();
-    let (tx, mut rx) = create_sub_channel(default_sub_capacity());
+    let (tx, _rx) = create_sub_channel(default_sub_capacity());
     let _sub = handle
         .subscribe("notice://realm/area/resource/meta".to_string(), tx, 1)
         .await
@@ -189,7 +199,7 @@ async fn should_deliver_notice_with_metadata() {
 async fn should_not_deliver_notice_to_unsubscribed_route() {
     // Arrange
     let (handle, _store) = start_test_engine();
-    let (tx, mut rx) = create_sub_channel(default_sub_capacity());
+    let (tx, _rx) = create_sub_channel(default_sub_capacity());
     let _sub = handle
         .subscribe("notice://realm/area/resource/foo".to_string(), tx, 1)
         .await
@@ -238,7 +248,7 @@ async fn should_handle_publish_when_no_subscribers_exist() {
 async fn should_not_receive_notices_after_unsubscribe() {
     // Arrange
     let (handle, _store) = start_test_engine();
-    let (tx, mut rx) = create_sub_channel(default_sub_capacity());
+    let (tx, _rx) = create_sub_channel(default_sub_capacity());
     let sub_id = handle
         .subscribe("notice://realm/area/resource/temp".to_string(), tx, 1)
         .await
@@ -303,7 +313,7 @@ async fn should_handle_channel_cleanup_for_nonexistent_channel() {
 async fn should_handle_subscriber_channel_full_backpressure() {
     // Arrange
     let (handle, _store) = start_test_engine();
-    let (tx, mut rx) = create_sub_channel(1);
+    let (tx, _rx) = create_sub_channel(1);
     let _sub = handle
         .subscribe("notice://realm/area/resource/burst".to_string(), tx, 1)
         .await
